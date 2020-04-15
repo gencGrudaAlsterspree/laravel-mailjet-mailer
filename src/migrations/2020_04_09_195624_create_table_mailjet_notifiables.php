184485cf0245bc064931c9146041101d09d413fe
wize-wiz <9093559+wize-wiz@users.noreply.github.com>
2020-04-15 18:20:36 +0200

    Add mailjet notifiables migration file.

---
+++ src/migrations/2020_04_09_195624_create_table_mailjet_notifiables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableMailjetRequests extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::dropIfExists('mailjet_notifiables');
+
+        Schema::create('mailjet_notifiables', function (Blueprint $table) {
+            $table->bigIncrements();
+            $table->string('mailjet_request_id', 36);
+            $table->morphs('mailjet_notifiable');
+            $table->timestamps();
+        });
+
+        Schema::table('mailjet_notifiables', function(Blueprint $table) {
+            $table->index('mailjet_request_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('mailjet_notifiables');
+    }
+}
